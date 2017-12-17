43fe1213d9c3a987f72ffda1822f40d18e0aa3d0
SHS-\SHS <pkh3715@naver.com>
2017-12-18 05:07:30 +0900

    스타트씬 추가, 인게임 화면에서 버튼 두개 있는데  누르고 있는 상태를 유지해야하는데 힘조절 하는게 클릭으로 밖에 안됨 방향조절 추가 해야됨

---
+++ mung/Assets/Scripts/StartScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour {
+
+    public bool isStart = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+    public void startGame()
+    {
+        isStart = true;
+    }
+}
